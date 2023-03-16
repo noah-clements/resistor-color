4d61375cfaca61ace4753eed8b94c3f3cedad2d2
noah-clements <noahclements@gmail.com>
2023-03-16 01:20:59 -0400

    refactored - took out external crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,75 +1,31 @@
-extern crate int_enum;
-extern crate enum_iterator;
+// Refactored to NOT use the external crates, even though that was the point of the exercise.
 
-use int_enum::IntEnum;
-// use int_enum::IntEnumError;
-use enum_iterator::{all, Sequence};
-
-#[repr(u32)]
-#[derive(Debug, PartialEq, Eq, IntEnum, Copy, Clone, Sequence)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 pub enum ResistorColor {
-    Black = 0,
-    Brown = 1,
-    Red = 2,
-    Orange = 3,
-    Yellow = 4,
-    Green = 5,
-    Blue = 6,
-    Violet = 7,
-    Grey = 8,
-    White = 9,
+    Black,
+    Brown,
+    Red,
+    Orange,
+    Yellow,
+    Green,
+    Blue,
+    Violet,
+    Grey,
+    White,
 }
 
-// Really should have more explanation for the IntEnum trait.
-// I thought we would have to implement it ourselves. And the docs were very unclear.
-// Instead we needed to put the values into the enum and the trait was implemented for us.
-// I had to look at other people's solutions to figure this out.
-/* impl IntEnum for ResistorColor {
-    type Int = u32;
-    fn int_value(self) -> u32 {
-        match self {
-            ResistorColor::Black => 0,
-            ResistorColor::Brown => 1,
-            ResistorColor::Red => 2,
-            ResistorColor::Orange => 3,
-            ResistorColor::Yellow => 4,
-            ResistorColor::Green => 5,
-            ResistorColor::Blue => 6,
-            ResistorColor::Violet => 7,
-            ResistorColor::Grey => 8,
-            ResistorColor::White => 9,
-        }
-    }
-
-    fn from_int(n: Self::Int) -> Result<Self, int_enum::IntEnumError<Self>>
-        where
-            Self: Sized {
-        match n {
-            0 => Ok(ResistorColor::Black),
-            1 => Ok(ResistorColor::Brown),
-            2 => Ok(ResistorColor::Red),
-            3 => Ok(ResistorColor::Orange),
-            4 => Ok(ResistorColor::Yellow),
-            5 => Ok(ResistorColor::Green),
-            6 => Ok(ResistorColor::Blue),
-            7 => Ok(ResistorColor::Violet),
-            8 => Ok(ResistorColor::Grey),
-            9 => Ok(ResistorColor::White),
-            _ => Err(int_enum::IntEnumError<_>),
-        }
-    } 
-}
- */
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    color.int_value()
+    colors().iter().position(|&c| c == color).unwrap() as u32
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-   ResistorColor::from_int(value)
-        .map(|c| format!("{:?}", c))
-        .unwrap_or_else(|_| "value out of range".to_owned())
+    if value > 9 {
+        return String::from("value out of range");
+    }
+    format!("{:?}", &colors()[value as usize])
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    all::<ResistorColor>().collect()
+    use ResistorColor::*;
+    vec![Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White]
 }
